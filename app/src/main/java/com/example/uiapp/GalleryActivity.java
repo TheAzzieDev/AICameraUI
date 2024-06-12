7db6096c65fa3114c8c8b0e3b0303caa5c9875a6
Jonathan Cano Sjöstedt <asrielrosemary@gmail.com>
2024-06-13 00:29:15 +0200

    finnaly a sticky header

--- app/src/main/java/com/example/uiapp/GalleryActivity.java
+++ app/src/main/java/com/example/uiapp/GalleryActivity.java
@@ -41,37 +41,30 @@ public class GalleryActivity extends AppCompatActivity {
         TextView stickyHeader = findViewById(R.id.stickyHeader);
         View scrollView = findViewById(R.id.scrollView);
 
-
-
-        TextView[] headers = new TextView[]{
-                findViewById(R.id.header1),
-                findViewById(R.id.header2),
-                findViewById(R.id.header5)
-                // Add more headers if you have more
-        };
         int scrollY = scrollView.getScrollY();
         LinearLayout linearLayout = findViewById(R.id.linearLayout);
         ArrayList<TextView> childViewData = new ArrayList<>();
 
         for (int i = 0; i < ( ((ViewGroup) linearLayout)).getChildCount(); i++) {
             View child = ((ViewGroup) linearLayout).getChildAt(i);
-            Object childId = child.getId();
-            childViewData.add(findViewById((int) childId));
-            Log.d("test2", childId.toString());
+            if(child instanceof TextView){
+                Object childId = child.getId();
+                childViewData.add(findViewById((int) childId));
+            }
         }
-
+        int count = 0;
+        int firstDistance = 0;
         TextView currentStickyHeader = null;
         for (TextView header : childViewData) {
             int[] location = new int[2];
             header.getLocationOnScreen(location);
             int headerTop = location[1] - scrollView.getTop();
-            stickyHeader.setText(String.format(Integer.toString(location[1]-scrollView.getTop())));
-
-            if (headerTop <= 0) {
-                currentStickyHeader = header;
-            } else {
-                break;
-            }
+            //stickyHeader.setText(String.format(Integer.toString(location[1]-scrollView.getTop())));
+            if (count == 0) firstDistance = headerTop;
+            count++;
+            if (headerTop <= 0) currentStickyHeader = header;
+            else if(firstDistance >= 0) stickyHeader.setText("");
+            else break;
         }
         if (currentStickyHeader != null) {
             stickyHeader.setText(currentStickyHeader.getText());
